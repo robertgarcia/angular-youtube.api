ca16835620c83398213ff5a0732bb23569d85a7b
Roberto <robertdavid.mgt@gmail.com>
2019-07-29 10:45:52 -0600

    Crear la interfaz de los videos del canal

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,22 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { YoutubeService } from '../../services/youtube.service';
 
+declare var $: any;
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styles: []
 })
 export class HomeComponent implements OnInit {
-
+  videos: any[] = [];
+  videoSel: any;
   constructor(
     public _yts: YoutubeService
   ) {
     _yts.getVideos().subscribe(resp => {
       console.log(resp);
+      this.videos = resp;
     });
   }
 
   ngOnInit() {
+  }
+
+  verVideo( video: any ) {
+    this.videoSel = video;
+    $('#exampleModal').modal();
   }
 
 }
--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -8,19 +8,19 @@ import { map } from 'rxjs/operators';
 })
 export class YoutubeService {
 
-  private youtubeUrl:string = "https://www.googleapis.com/youtube/v3/";
-  private apiKey:string = "AIzaSyCuZ9nnarxBY4FEwp8CBcxciBSK9Jbyb0A";
-  private playlist:string = "UUuaPTYj15JSkETGnEseaFFg";
-  private nextPageToken:string = "";
+  private youtubeUrl = 'https://www.googleapis.com/youtube/v3/';
+  private apiKey = 'AIzaSyCuZ9nnarxBY4FEwp8CBcxciBSK9Jbyb0A';
+  private playlist = 'UUuaPTYj15JSkETGnEseaFFg';
+  private nextPageToken = '';
 
   constructor(
-    public http:Http,
-    public httpClient:HttpClient
+    public http: Http,
+    public httpClient: HttpClient
   ) { }
 
-  getVideos(){
-    let url = `${ this.youtubeUrl }playlistItems?part=snippet&maxResults=10&playlistId=${ this.playlist }&key=${ this.apiKey }`;
-    
+  getVideos() {
+    const url = `${ this.youtubeUrl }playlistItems?part=snippet&maxResults=10&playlistId=${ this.playlist }&key=${ this.apiKey }`;
+
     /*let url = `${ this.youtubeUrl }playlistItems`;
     let params: URLSearchParams = new URLSearchParams();
     params.set('part', 'snippet');
@@ -28,13 +28,13 @@ export class YoutubeService {
     params.set('playlistId', this.playlist);
     params.set('key', this.apiKey);*/
 
-    return this.httpClient.get( url ).pipe(map( (resp:any) => {
-      //console.log(resp['nextPageToken']);
-      this.nextPageToken = resp['nextPageToken'];
-      let videos:any[] = [];
-      for(let video of resp.items){
+    return this.httpClient.get( url ).pipe(map( (resp: any) => {
+      /*console.log(resp['nextPageToken']);*/
+      this.nextPageToken = resp.nextPageToken;
+      const videos: any[] = [];
+      for (const video of resp.items) {
         //console.log(video.snippet);
-        videos.push(video.snippet)
+        videos.push(video.snippet);
       }
       return videos;
     }));
